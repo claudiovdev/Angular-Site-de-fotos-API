245a17d10842e3546f9bad706c06ca76c02c6687
viniciuscfn <c.viniciusmagalhaes@gmail.com>
2021-09-12 19:40:23 -0300

    Configurando  rotas e responsividades do Header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,20 @@
+import { HeaderComponent } from './shared/components/template/header/header.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { ListarImagensComponent } from './imagens/listar-imagens/listar-imagens.component';
 
+const routes: Routes = [
+  {path: '', component: ListarImagensComponent},
+
+];
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
