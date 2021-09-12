88abe3c1e2026c42a7f15a3bf6d6989492d08097
viniciuscfn <c.viniciusmagalhaes@gmail.com>
2021-09-12 15:46:10 -0300

    Shared
    Criando meus components e models

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './shared/components/template/nav/nav.component';
+import { HeaderComponent } from './shared/components/template/header/header.component';
+import { FooterComponent } from './shared/components/template/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
